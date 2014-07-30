2120bd5ab3762a994db1194b750af179dacbfbdb
Arne Vandamme <arne@foreach.be>
2014-07-30 13:36:26 +0200

    Add module conventions tests

---
+++ user-module/src/test/java/com/foreach/across/modules/user/TestUserModuleConventions.java
@@ -0,0 +1,20 @@
+package com.foreach.across.modules.user;
+
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.test.AbstractAcrossModuleConventionsTest;
+
+/**
+ * @author Arne Vandamme
+ */
+public class TestUserModuleConventions extends AbstractAcrossModuleConventionsTest
+{
+	@Override
+	protected boolean hasSettings() {
+		return true;
+	}
+
+	@Override
+	protected AcrossModule createModule() {
+		return new UserModule();
+	}
+}
