e01971eab0967c8893ab11e9dd67af9725a01416
Arne Vandamme <arne@foreach.be>
2014-07-30 14:38:53 +0200

    Update sequence table for new generator

--- user-module/src/main/java/com/foreach/across/modules/user/installers/UserSchemaInstaller.java
+++ user-module/src/main/java/com/foreach/across/modules/user/installers/UserSchemaInstaller.java
@@ -4,7 +4,7 @@ import com.foreach.across.core.annotations.Installer;
 import com.foreach.across.core.database.SchemaConfiguration;
 import com.foreach.across.core.installers.AcrossLiquibaseInstaller;
 
-@Installer(description = "Installs database schema for user authorization.", version = 10)
+@Installer(description = "Installs database schema for user authorization.", version = 11)
 public class UserSchemaInstaller extends AcrossLiquibaseInstaller
 {
 	public UserSchemaInstaller( SchemaConfiguration schemaConfiguration ) {
