07a8f1655d9a734896711c963042597bd5552db5
WimTibackx <tibackx.wim@gmail.com>
2014-11-28 12:10:02 +0100

    Extract the BasicRepositoryInterceptor and EntityInterceptor.

--- user-module/src/main/java/com/foreach/across/modules/user/services/GroupAclInterceptor.java
+++ user-module/src/main/java/com/foreach/across/modules/user/services/GroupAclInterceptor.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.foreach.across.modules.user.services;
 
 import com.foreach.across.core.annotations.AcrossDepends;
-import com.foreach.across.modules.spring.security.acl.business.AclPermission;
 import com.foreach.across.modules.spring.security.acl.business.AclSecurityEntity;
 import com.foreach.across.modules.spring.security.acl.services.AclSecurityEntityService;
 import com.foreach.across.modules.spring.security.acl.support.IdBasedEntityAclInterceptor;
@@ -9,7 +23,7 @@ import com.foreach.across.modules.user.UserModuleSettings;
 import com.foreach.across.modules.user.business.Group;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@AcrossDepends(required = "SpringSecurityAclModule")
+@AcrossDepends(required = { "SpringSecurityAclModule", "AcrossHibernateModule" })
 public class GroupAclInterceptor extends IdBasedEntityAclInterceptor<Group>
 {
 	@Autowired
@@ -20,7 +34,7 @@ public class GroupAclInterceptor extends IdBasedEntityAclInterceptor<Group>
 
 	@Override
 	public void afterCreate( Group entity ) {
-		if (settings.isEnableDefaultAcls()) {
+		if ( settings.isEnableDefaultAcls() ) {
 			AclSecurityEntity groupParentEntity = aclSecurityEntityService.getSecurityEntityByName( "groups" );
 			aclSecurityService().createAclWithParent( entity, groupParentEntity );
 		}
@@ -32,7 +46,7 @@ public class GroupAclInterceptor extends IdBasedEntityAclInterceptor<Group>
 
 	@Override
 	public void beforeDelete( Group entity, boolean isSoftDelete ) {
-		if (settings.isEnableDefaultAcls()) {
+		if ( settings.isEnableDefaultAcls() ) {
 			aclSecurityService().deleteAcl( entity, true );
 		}
 	}
