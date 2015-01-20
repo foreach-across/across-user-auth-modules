9ae2621b8b8244e89e2579095ebd78fc39036af9
Arne Vandamme <arne@foreach.be>
2015-01-20 20:54:45 +0100

    Optimize list overview - switch to message codes (WIP)

--- user-module/src/main/java/com/foreach/across/modules/user/config/modules/UserEntitiesConfiguration.java
+++ user-module/src/main/java/com/foreach/across/modules/user/config/modules/UserEntitiesConfiguration.java
@@ -3,6 +3,7 @@ package com.foreach.across.modules.user.config.modules;
 import com.foreach.across.core.annotations.AcrossDepends;
 import com.foreach.across.modules.entity.config.EntitiesConfigurationBuilder;
 import com.foreach.across.modules.entity.config.EntityConfigurer;
+import com.foreach.across.modules.entity.views.EntityListView;
 import com.foreach.across.modules.user.business.Role;
 import com.foreach.across.modules.user.business.User;
 import org.springframework.context.annotation.Configuration;
@@ -20,27 +21,27 @@ public class UserEntitiesConfiguration implements EntityConfigurer
 						.property( "lastModified", "Last modified", new AuditableLastModifiedPrinter() ).and()
 						.hide( "createdDate", "createdBy", "lastModifiedDate", "lastModifiedBy" )
 						.and()*/
-		             .view( "crud-list" )
-		             .properties(
-				             "id",
-				             "email",
-				             "displayName",
-				             "group-membership",
-				             "role-membership",
-				             "createdDate",
-				             "createdBy",
-				             "lastModifiedDate",
-				             "lastModifiedBy"
-		             )
-		             .property( "group-membership", "Groups", "groups.size()" )
-		             .property( "role-membership", "Roles", "roles.size()" );
+				.view( EntityListView.VIEW_NAME )
+				.properties(
+						"id",
+						"email",
+						"displayName",
+						"group-membership",
+						"role-membership",
+						"createdDate",
+						"createdBy",
+						"lastModifiedDate",
+						"lastModifiedBy"
+				)
+				.property( "group-membership", "Groups", "groups.size()" )
+				.property( "role-membership", "Roles", "roles.size()" );
 
 		configuration.entity( Role.class )
 		             .properties()
 		             .property( "name", "Key" )
 		             .property( "description", "Name" )
 		             .and()
-		             .view( "crud-list" ).properties( "description", "name" );
+		             .view( EntityListView.VIEW_NAME ).properties( "description", "name" );
 
 	}
 }
