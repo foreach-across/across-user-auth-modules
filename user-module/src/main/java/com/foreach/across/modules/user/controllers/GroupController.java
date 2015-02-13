071ba0c1a1d48701e814e9e5507ed73faedd5117
Arne Vandamme <arne@foreach.be>
2015-02-13 17:34:21 +0100

    Support OneToMany mappings

--- user-module/src/main/java/com/foreach/across/modules/user/controllers/GroupController.java
+++ user-module/src/main/java/com/foreach/across/modules/user/controllers/GroupController.java
@@ -1,13 +1,25 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.foreach.across.modules.user.controllers;
 
-import com.foreach.across.core.annotations.Event;
 import com.foreach.across.modules.adminweb.AdminWeb;
 import com.foreach.across.modules.adminweb.annotations.AdminWebController;
 import com.foreach.across.modules.adminweb.menu.AdminMenu;
 import com.foreach.across.modules.adminweb.menu.EntityAdminMenu;
-import com.foreach.across.modules.adminweb.menu.EntityAdminMenuEvent;
 import com.foreach.across.modules.user.business.Group;
-import com.foreach.across.modules.user.business.GroupedPrincipal;
 import com.foreach.across.modules.user.services.GroupService;
 import com.foreach.across.modules.user.services.RoleService;
 import com.foreach.across.modules.web.menu.MenuFactory;
@@ -37,14 +49,14 @@ public class GroupController
 	@Autowired
 	private MenuFactory menuFactory;
 
-	@Event
-	protected void registerGroupsTab( EntityAdminMenuEvent<GroupedPrincipal> menu ) {
-		if ( menu.isForUpdate() ) {
-			menu.builder().item( "groups", "Groups",
-			                     "/entities/" + ( menu.getEntityType().getSimpleName().toLowerCase() ) + "/" + menu
-					                     .getEntity().getId() + "/groups" );
-		}
-	}
+//	@Event
+//	protected void registerGroupsTab( EntityAdminMenuEvent<GroupedPrincipal> menu ) {
+//		if ( menu.isForUpdate() ) {
+//			menu.builder().item( "groups", "Groups",
+//			                     "/entities/" + ( menu.getEntityType().getSimpleName().toLowerCase() ) + "/" + menu
+//					                     .getEntity().getId() + "/groups" );
+//		}
+//	}
 
 	@RequestMapping
 	public String listGroups( Model model ) {
