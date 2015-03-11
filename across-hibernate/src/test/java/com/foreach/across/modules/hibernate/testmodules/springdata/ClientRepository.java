6ba9a12fa6d3359bca7d1762476f5d25bb5a28e8
Arne Vandamme <arne@foreach.be>
2015-03-11 17:46:28 +0100

    LabelFormElement and render radio and checkbox according to bootstrap

--- across-hibernate/src/test/java/com/foreach/across/modules/hibernate/testmodules/springdata/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foreach.across.modules.hibernate.testmodules.springdata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long>
-{
-}
