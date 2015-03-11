b257dcdb1f5f66c922557e1a27f187c8e890456f
Arne Vandamme <arne@foreach.be>
2015-03-11 21:03:02 +0100

    Bootstrap grid helper to generate css classes

---
+++ entity-module/src/main/java/com/foreach/across/modules/entity/registry/properties/EntityPropertyFilter.java
@@ -0,0 +1,22 @@
+package com.foreach.across.modules.entity.registry.properties;
+
+import java.util.Collection;
+
+public interface EntityPropertyFilter
+{
+	boolean shouldInclude( EntityPropertyDescriptor descriptor );
+
+	Collection<String> getPropertyNames();
+
+	/**
+	 * Sub interface stating that the property filter declares all included properties explicitly.
+	 */
+	static interface Inclusive extends EntityPropertyFilter {
+	}
+
+	/**
+	 * Sub interface stating that the property filter only declares properties that should be excluded.
+	 */
+	static interface Exclusive extends EntityPropertyFilter {
+	}
+}
