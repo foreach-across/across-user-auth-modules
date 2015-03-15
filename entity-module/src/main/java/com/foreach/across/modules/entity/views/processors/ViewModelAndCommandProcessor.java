1a5e361213bc3c23cca6035175e3a0b8dc5858f8
Arne Vandamme <arne@foreach.be>
2015-03-15 21:29:20 +0100

    Start of BootstrapUiFactory

---
+++ entity-module/src/main/java/com/foreach/across/modules/entity/views/processors/ViewModelAndCommandProcessor.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.modules.entity.views.processors;
+
+import com.foreach.across.modules.entity.controllers.EntityViewCommand;
+import com.foreach.across.modules.entity.views.ViewCreationContext;
+import org.springframework.ui.ModelMap;
+
+/**
+ * @author Arne Vandamme
+ */
+public interface ViewModelAndCommandProcessor<T extends ViewCreationContext>
+{
+	void prepareModelAndCommand( String viewName,
+	                             T creationContext,
+	                             EntityViewCommand command,
+	                             ModelMap model );
+}
