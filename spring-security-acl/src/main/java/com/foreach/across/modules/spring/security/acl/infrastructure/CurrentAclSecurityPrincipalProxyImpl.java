eff8eec1ae692b76ee5d020e343ce7da0979de70
Arne Vandamme <arne@foreach.be>
2015-07-31 13:11:56 +0200

    Upgrade to JDK 8 - remove ambiguous methods

--- spring-security-acl/src/main/java/com/foreach/across/modules/spring/security/acl/infrastructure/CurrentAclSecurityPrincipalProxyImpl.java
+++ spring-security-acl/src/main/java/com/foreach/across/modules/spring/security/acl/infrastructure/CurrentAclSecurityPrincipalProxyImpl.java
@@ -19,6 +19,7 @@ import com.foreach.across.core.annotations.Refreshable;
 import com.foreach.across.modules.hibernate.business.IdBasedEntity;
 import com.foreach.across.modules.spring.security.acl.business.AclPermission;
 import com.foreach.across.modules.spring.security.acl.services.AclSecurityService;
+import com.foreach.across.modules.spring.security.infrastructure.business.SecurityPrincipal;
 import com.foreach.across.modules.spring.security.infrastructure.services.CurrentSecurityPrincipalProxyImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,6 +38,6 @@ public class CurrentAclSecurityPrincipalProxyImpl extends CurrentSecurityPrincip
 	public boolean hasAclPermission( IdBasedEntity entity, AclPermission permission ) {
 		return aclSecurityService != null
 				&& isAuthenticated()
-				&& aclSecurityService.hasPermission( getPrincipal(), entity, permission );
+				&& aclSecurityService.hasPermission( getPrincipal( SecurityPrincipal.class ), entity, permission );
 	}
 }
