415f0c2c2a1a6245b92ddcc78fa66158f0d1e492
Arne Vandamme <arne@foreach.be>
2015-08-18 18:53:08 +0200

    AXSTD-117 fix unit test attempt 2

--- properties-module/src/test/java/com/foreach/across/modules/properties/TestPropertiesModuleConventions.java
+++ properties-module/src/test/java/com/foreach/across/modules/properties/TestPropertiesModuleConventions.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2014 the original author or authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,23 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.foreach.across.modules.entity.registry.properties;
+package com.foreach.across.modules.properties;
+
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.test.AbstractAcrossModuleConventionsTest;
 
 /**
  * @author Arne Vandamme
  */
-public interface EntityPropertyRegistryFactory
+public class TestPropertiesModuleConventions extends AbstractAcrossModuleConventionsTest
 {
-	MutableEntityPropertyRegistry getOrCreate( Class<?> entityType );
+	@Override
+	protected boolean hasSettings() {
+		return true;
+	}
 
-	EntityPropertyRegistry get( Class<?> entityType );
-
-	/**
-	 * Creates a new registry that uses the existing as parent.  Will usually return a
-	 * {@link MergingEntityPropertyRegistry} where the property descriptors are also inherited.
-	 *
-	 * @param entityPropertyRegistry that is the parent
-	 * @return new registry instance for the parent source
-	 */
-	MutableEntityPropertyRegistry createWithParent( EntityPropertyRegistry entityPropertyRegistry );
+	@Override
+	protected AcrossModule createModule() {
+		return new PropertiesModule();
+	}
 }
