030385bed4c5512ea759b64103c963b6aa75a890
arne <arne@foreach.be>
2016-03-17 08:38:08 +0100

    Initial project

---
+++ ldap-module/src/main/java/com/foreach/across/modules/ldap/LdapModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.foreach.across.modules.ldap;
+
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.annotations.AcrossDepends;
+import com.foreach.across.modules.web.AcrossWebModule;
+
+/**
+ * @author Arne Vandamme
+ * @since 1.0.0
+ */
+@AcrossDepends(
+        required = AcrossWebModule.NAME,
+        optional = {"AdminWebModule", "EntityModule", "UserModule"}
+)
+public class LdapModule extends AcrossModule {
+    public static final String NAME = "LdapModule";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Provides a domain model and services for connecting to LDAP directories.";
+    }
+}
