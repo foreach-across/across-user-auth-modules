c66c8147adbfe27d816574fd7196968da7b2c87d
Arne Vandamme <arne@foreach.be>
2018-01-05 11:17:15 +0100

    Add test project for admin UI rendering

---
+++ test-admin-ui/src/main/java/test/acl/AdminUiApplication.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test.acl;
+
+import com.foreach.across.config.AcrossApplication;
+import com.foreach.across.modules.adminweb.AdminWebModule;
+import com.foreach.across.modules.entity.EntityModule;
+import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
+import com.foreach.across.modules.spring.security.acl.SpringSecurityAclModule;
+import com.foreach.across.modules.web.AcrossWebModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+/**
+ * @author Arne Vandamme
+ * @since 3.0.0
+ */
+@AcrossApplication(
+		modules = {
+				AcrossWebModule.NAME,
+				AdminWebModule.NAME,
+				AcrossHibernateJpaModule.NAME,
+				EntityModule.NAME,
+				SpringSecurityAclModule.NAME
+		}
+)
+@Import({ DataSourceAutoConfiguration.class, H2ConsoleAutoConfiguration.class })
+@EnableSpringDataWebSupport
+public class AdminUiApplication
+{
+	public static void main( String[] args ) {
+		SpringApplication springApplication = new SpringApplication( AdminUiApplication.class );
+		springApplication.run( args );
+	}
+}
