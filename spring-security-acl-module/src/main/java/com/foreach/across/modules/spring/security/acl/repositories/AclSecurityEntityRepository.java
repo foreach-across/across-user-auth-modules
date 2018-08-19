9dfeab5910aef9728d97ca68af93baa89b2e5dbc
Arne Vandamme <arne@foreach.be>
2018-08-19 13:45:27 +0200

    Fix NPE on cache expressions

--- spring-security-acl-module/src/main/java/com/foreach/across/modules/spring/security/acl/repositories/AclSecurityEntityRepository.java
+++ spring-security-acl-module/src/main/java/com/foreach/across/modules/spring/security/acl/repositories/AclSecurityEntityRepository.java
@@ -31,8 +31,8 @@ public interface AclSecurityEntityRepository extends IdBasedEntityJpaRepository<
 {
 	@Caching(
 			put = {
-					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.id"),
-					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.name")
+					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.id", condition = "#result != null"),
+					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.name", condition = "#result != null")
 			}
 	)
 	@Override
@@ -40,8 +40,8 @@ public interface AclSecurityEntityRepository extends IdBasedEntityJpaRepository<
 
 	@Caching(
 			put = {
-					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.id"),
-					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.name")
+					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.id", condition = "#result != null"),
+					@CachePut(value = SpringSecurityAclModuleCache.ACL_SECURITY_ENTITY, key = "#result.name", condition = "#result != null")
 			}
 	)
 	Optional<AclSecurityEntity> findByName( String name );
