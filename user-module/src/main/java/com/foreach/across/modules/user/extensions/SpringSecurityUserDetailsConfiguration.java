f992e45e6b4093bc6ed02e45fbed222a972b445a
Davy Verstappen <davy.verstappen@iodigital.com>
2024-04-16 19:02:19 +0200

    Attempt to create AuthenticationManager beans directly, instead of using AuthenticationManagerBuilder:
    https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html#_publish_an_authenticationmanager_bean
    
    But could not get this to work with the tests in across-platform.
    Keeping the commented code as documentation of the attempted options.

--- user-module/src/main/java/com/foreach/across/modules/user/extensions/SpringSecurityUserDetailsConfiguration.java
+++ user-module/src/main/java/com/foreach/across/modules/user/extensions/SpringSecurityUserDetailsConfiguration.java
@@ -58,4 +58,65 @@ class SpringSecurityUserDetailsConfiguration
 		// Configure the global user details for remember me functionality
 		auth.userDetailsService( userDetailsService ).passwordEncoder( userPasswordEncoder );
 	}
+
+	// I haven't been able to figure out a way to get rid of the AuthenticationManagerBuilder above; see:
+	// https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html#_publish_an_authenticationmanager_bean
+
+	/*
+	// This naive option 1 doesn't work:
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		return contextBeanRegistry.getBeanOfTypeFromModule(
+				UserModule.NAME,
+				UserDirectoryAuthenticationProvider.class
+		);
+	}
+
+	// (Option 1b) But is slightly better with this removed:
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return contextBeanRegistry.getBeanOfTypeFromModule(
+				UserModule.NAME,
+				PasswordEncoder.class
+		);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return contextBeanRegistry.getBeanOfTypeFromModule(
+				UserModule.NAME,
+				UserDetailsService.class
+		);
+	}
+	*/
+
+	/*
+	// (Option 2) This is as close as I can get to the AuthenticationManager that is created by the AuthenticationManagerBuilder,
+	// but the ITPlatformTestApplication.adminWebModuleListsUserOverviewForAuthenticatedUser() fails with:
+	//   java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
+	//	 at org.springframework.security.crypto.password.DelegatingPasswordEncoder$UnmappedIdPasswordEncoder.matches(DelegatingPasswordEncoder.java:289)
+	//	 at org.springframework.security.crypto.password.DelegatingPasswordEncoder.matches(DelegatingPasswordEncoder.java:237)
+	//	 at org.springframework.security.authentication.dao.DaoAuthenticationProvider.additionalAuthenticationChecks(DaoAuthenticationProvider.java:77)
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		PasswordEncoder userPasswordEncoder = contextBeanRegistry.getBeanOfTypeFromModule(
+				UserModule.NAME,
+				PasswordEncoder.class
+		);
+		UserDetailsService userDetailsService = contextBeanRegistry.getBeanOfTypeFromModule(
+				UserModule.NAME,
+				UserDetailsService.class
+		);
+
+		UserDirectoryAuthenticationProvider userDirectoryAuthenticationProvider
+				= contextBeanRegistry.getBeanOfTypeFromModule( UserModule.NAME,
+				                                               UserDirectoryAuthenticationProvider.class );
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder( userPasswordEncoder );
+		daoAuthenticationProvider.setUserDetailsService( userDetailsService );
+		return new ProviderManager( userDirectoryAuthenticationProvider, daoAuthenticationProvider );
+	}
+*/
+
 }
